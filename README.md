ad56234ed70ed3d4590977835deb64ae804b2bf4
Pol Pages <polpages1999@gmail.com>
2023-11-22 20:25:36 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # 2048 Game
-
 ## Project for the _Test & Qualitat del Software_ class.
-
 ### 2048 game developed with JavaScript following a TDD implementation.
 
 >2048 is a game where you combine numbered tiles in order to gain a higher numbered tile. In this game you start with two tiles, the lowest possible number available is two. Then you will play by combining the tiles with the same number to have a tile with the sum of the number on the two tiles.
@@ -12,6 +10,9 @@ Developed by:
 - Pol Pages   – **1494769**
 - Sergi Vila  – **1531267**
 
+<hr />
+
+# Tests implemented
 ### Equivalent partitions tests
 #### Board
 1. You should be able to initialize a board with empty cells.
--- test/Board.test.js
+++ test/Board.test.js
@@ -23,7 +23,7 @@ describe('Equivalent partitions', () => {
         expect(tileCount).toBe(2);
     });
 
-    // 3.1. After a player moves left, the tiles should shift in the direction of the move, combining equal tiles.
+    // 3. After a player moves left, the tiles should shift in the direction of the move, combining equal tiles.
     test('should move tiles to the left when the left move is made', () => {
         const gameBoard = new Board();
         gameBoard.board = [
@@ -36,7 +36,7 @@ describe('Equivalent partitions', () => {
         expect(gameBoard.board[0]).toEqual([4, 4, 0, 0]);
     });
 
-    // 3.2. After a player moves right, the tiles should shift in the direction of the move, combining equal tiles.
+    // 4. After a player moves right, the tiles should shift in the direction of the move, combining equal tiles.
     test('should move tiles to the right when the right move is made', () => {
         const gameBoard = new Board();
         gameBoard.board = [
@@ -49,7 +49,7 @@ describe('Equivalent partitions', () => {
         expect(gameBoard.board[0]).toEqual([0, 0, 4, 4]);
     });
 
-    // 3.3. After a player moves up, the tiles should shift in the direction of the move, combining equal tiles.
+    // 5. After a player moves up, the tiles should shift in the direction of the move, combining equal tiles.
     test('should move tiles up when the up move is made', () => {
         const gameBoard = new Board();
         gameBoard.board = [
@@ -62,7 +62,7 @@ describe('Equivalent partitions', () => {
         expect(gameBoard.board[0]).toEqual([0, 0, 4, 4]);
     });
 
-    // 3.4. After a player moves down, the tiles should shift in the direction of the move, combining equal tiles.
+    // 6. After a player moves down, the tiles should shift in the direction of the move, combining equal tiles.
     test('should move tiles down when the down move is made', () => {
         const gameBoard = new Board();
         gameBoard.board = [
@@ -75,7 +75,7 @@ describe('Equivalent partitions', () => {
         expect(gameBoard.board[3]).toEqual([0, 0, 4, 4]);
     })
 
-    // 4. A test to check if the combining of tiles works correctly. For example, when two '2' tiles combine, they should become one '4' tile.
+    // 7. A test to check if the combining of tiles works correctly. For example, when two '2' tiles combine, they should become one '4' tile.
     test('should combine tiles correctly when moved', () => {
         const gameBoard = new Board();
         gameBoard.board = [
@@ -89,7 +89,7 @@ describe('Equivalent partitions', () => {
         expect(gameBoard.board[1]).toEqual([8, 0, 0, 0]);
     });
 
-    // 5. A test to determine if the game correctly identifies when no moves are left, meaning the game is over.
+    // 8. A test to determine if the game correctly identifies when no moves are left, meaning the game is over.
     test('should determine if no moves are left', () => {
         const gameBoard = new Board();
         gameBoard.board = [
@@ -101,4 +101,70 @@ describe('Equivalent partitions', () => {
         const noMovesLeft = gameBoard.checkGameOver();
         expect(noMovesLeft).toBeTruthy();
     });
+});
+
+/* ---------- Limit and frontier values ---------- */
+
+describe('Limit and frontier values', () => {
+    
+    // 9. Check if the game recognizes a win when 2048 is reached.
+    test('should recognize a win when 2048 tile is reached', () => {
+        const gameBoard = new Board();
+        gameBoard.board = [
+          [1024, 1024, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]
+        ];
+        gameBoard.moveLeft();
+        expect(gameBoard.hasWon()).toBeTruthy();
+      });
+
+      // 10. Verify that the game identifies when no horizontal moves are possible.
+      test('should identify no horizontal moves left', () => {
+        const gameBoard = new Board();
+        gameBoard.board = [
+          [2, 4, 2, 4],
+          [4, 2, 4, 2],
+          [2, 4, 2, 4],
+          [4, 2, 4, 2]
+        ];
+        expect(gameBoard.canMoveHorizontal()).toBeFalsy();
+      });
+
+      // 11. Confirm that the game recognizes when no vertical moves can be made.
+      test('should identify no vertical moves left', () => {
+        const gameBoard = new Board();
+        gameBoard.board = [
+          [2, 4, 2, 4],
+          [4, 2, 4, 2],
+          [2, 4, 2, 4],
+          [4, 2, 4, 2]
+        ];
+        expect(gameBoard.canMoveVertical()).toBeFalsy();
+      });
+
+      // 12. Ensure the game continues if the board is full but combinations are possible.
+      test('should not end game if the board is full but moves are available', () => {
+        const gameBoard = new Board();
+        gameBoard.board = [
+          [2, 4, 2, 4],
+          [4, 2, 4, 2],
+          [2, 4, 2, 4],
+          [4, 2, 4, 8]
+        ];
+        expect(gameBoard.checkGameOver()).toBeFalsy();
+      });
+
+      //13. Identify the highest tile after the game is over.
+      test('should report the highest tile after game over', () => {
+        const gameBoard = new Board();
+        gameBoard.board = [
+          [1024, 64, 32, 16],
+          [512, 256, 128, 64],
+          [256, 128, 64, 32],
+          [128, 64, 32, 16]
+        ];
+        expect(gameBoard.getHighestTile()).toBe(1024);
+      });      
 });

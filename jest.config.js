5553daf76f10bd0e7f83488f6b0bc0b00c144a4a
Pol Pages <polpages1999@gmail.com>
2023-11-23 20:29:28 +0100

    First coverage report generated. TODO: Implement remaining tests to ensure full coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
     transform: {
         '^.+\\.[t|j]sx?$': 'babel-jest',
     },
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.js"],
+    coverageDirectory: "./coverage",
+    coverageReporters: ["text", "lcov"]
 };
